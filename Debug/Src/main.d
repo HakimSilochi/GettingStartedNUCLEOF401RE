61829011d7fc6ebb1208d5fbab89537e51033f92
Hakim Epea Silochi <hakimepeasilochi@gmail.com>
2022-09-08 23:02:59 +0800

    Starting with the board.

---
+++ Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Device/ST/STM32F4xx/Include/stm32f410rx.h \
+ D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Include/core_cm4.h \
+ D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Include/cmsis_version.h \
+ D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Include/cmsis_compiler.h \
+ D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Include/cmsis_gcc.h \
+ D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Include/mpu_armv7.h \
+ D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Device/ST/STM32F4xx/Include/stm32f410rx.h:
+D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Include/core_cm4.h:
+D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Include/cmsis_version.h:
+D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Include/cmsis_compiler.h:
+D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Include/cmsis_gcc.h:
+D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Include/mpu_armv7.h:
+D:/STMIDE/NUCLEO-F401RE/001LedsBareMetalF401RE/DRIVERS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ Src/main.c
@@ -0,0 +1,28 @@
+#include "stm32f410rx.h"
+//Una vez incluidas las librerias de cmsis procedemos a activar el reloj del led que queremos utilizar.
+void delayMs (int n);
+int  main (){
+//Led esta en PA5 y el boton se encuentra en el PC13 con una resistencia pull uo que garantiza un nivel alto si no se pulsa el boton.
+RCC->AHB1ENR |= (1<<0)|(1<<2); //ACTIVACION DE CLOCKS
+GPIOA->MODER &=~(1<<11);
+GPIOA->MODER |=(1<<10); //LED COMO SALIDA.
+GPIOA->MODER &=~(1<<5);
+	while (1){
+		//El led se enciende con una signal positiva.
+		GPIOA->BSRR =(1<<5);
+		delayMs(20);
+		GPIOA->BSRR =(1<<21);
+		delayMs(20);
+
+
+
+    }
+
+
+return 0;
+
+}
+void delayMs(int n) {
+int i;
+for (; n > 0; n--)
+for (i = 0; i < 3195; i++) ; }
